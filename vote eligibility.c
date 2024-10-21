b1e7332f476c825734a22b606a9021354544f291
shadowwalker390 <katakamleeladurgaprasad@gmail.com>
2024-10-21 11:24:25 +0530

    Create vote eligibility.c

---
+++ vote eligibility.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int age;
+    printf("Enter the age:");
+    scanf("%d",&age);
+    if(age>=18)
+    printf("YOU ARE ELIGIBLE TO VOTE");
+    else
+    printf("YOU ARE NOT ELIGIBLE TO VOTE");
+return 0;
+}
