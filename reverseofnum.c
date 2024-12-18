f24a88a6835641533b7315e0e1547f424ca72a22
shadowwalker390 <katakamleeladurgaprasad@gmail.com>
2024-12-18 15:46:45 +0530

    reverseofnum.c

---
+++ reverseofnum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,rem,rev=0;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    while(n>0){
+        rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+    }
+    printf("The Reverse of number is %d",rev);
+}
